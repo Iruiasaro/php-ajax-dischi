0e3a549c55774b47959266aaf63f235225489db9
Iruiasaro <brunettirosario@gmail.com>
2021-06-03 18:46:36 +0200

    ciclo for each in html per risultato raccolta php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-<?php 
-include "raccolta.php"
+<?php
+include "./raccolta.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,7 +12,18 @@ include "raccolta.php"
     <link rel="stylesheet" href="dist/app.css">
     <title>PHP_AJAX_DISCHI</title>
 </head>
+
 <body>
-    
+        <!-- ciclo for each per restituire risultato -->
+            <?php foreach ($playlist as $item) { ?>
+                    <img src="<?php echo $item['poster']; ?>" alt="">
+                    <h3> <?php echo $item['title']; ?> </h3>
+                    <h4> <?php echo $item['author']; ?> </h4>
+                    <h4> <?php echo $item['genre']; ?> </h4>
+                    <h4> <?php echo $item['year']; ?> </h4>
+
+            <?php } ?>
+            </ul>
 </body>
+
 </html>
