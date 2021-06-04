db2c938ae56c868139667a928f975e05de915c18
Iruiasaro <brunettirosario@gmail.com>
2021-06-04 18:37:20 +0200

    milestone 2 - inserisce album tramite VueJS

---
+++ php-ajax-dischi-two/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/app.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <title>Php_Ajax_Dischi_Two</title>
+</head>
+<body>
+    <div id="root">
+        <div class="wrapper">
+            <div v-for="song in playlist">
+                <img :src="song.poster" alt="">
+                <h4>{{song.title}}</h4>
+                <span>{{song.author}}</span>
+                <span>{{song.genre}}</span>
+                <span>{{song.year}}</span>
+            </div>
+        </div>
+    </div>
+    <script src="scripts/app.js"></script>
+</body>
+</html>
